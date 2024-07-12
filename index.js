256271bd14f1bc90e54c67ce7f917b30309f17c7
lester <lesterc013@gmail.com>
2024-07-12 10:28:13 +0800

    PUT request

--- index.js
+++ index.js
@@ -48,8 +48,29 @@ app.post(baseUrl, (request, response) => {
   }
 })
 
+// PUT a todo
+app.put(`${baseUrl}/:id`, (request, response) => {
+  const id = parseInt(request.params.id)
+  const update = request.body
+  // Invalid id in url
+  if (!todos.find((todo) => todo.id === id)) {
+    response.status(400).json({
+      error: 'invalid id',
+    })
+  }
+  // Task is empty
+  else if (update.task.length === 0) {
+    response.status(400).json({
+      error: 'task not provided',
+    })
+  } else {
+    todos = todos.map((todo) => (todo.id === id ? update : todo))
+    response.status(200).json(update)
+  }
+})
+
 // GET single todo based on id
-app.get(`${baseUrl}:id`, (request, response) => {
+app.get(`${baseUrl}/:id`, (request, response) => {
   // Use the id and filter out the matchin todo
   const id = parseInt(request.params.id)
   const todo = todos.filter((todo) => todo.id === id)
