f7ff9e7556e8d05ee890bb5ef5cee276087abf53
lester <lesterc013@gmail.com>
2024-07-18 23:30:14 +0800

    uibuild

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./app')
 const config = require('./utils/config')
 
 // Listener
-app.listen(config.PORT, () => {
+const PORT = config.PORT || 3001
+app.listen(PORT, () => {
   console.log('Server running on', config.PORT)
 })
