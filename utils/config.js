2044aa3c74bf167b02a7aedc36c360ba6a4a4e9f
lester <lesterc013@gmail.com>
2024-07-19 11:31:43 +0800

    Added use of encryption and signature key

--- utils/config.js
+++ utils/config.js
@@ -5,8 +5,12 @@ const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
+const encKey = process.env.ENC_KEY
+const sigKey = process.env.SIG_KEY
 
 module.exports = {
   PORT,
   MONGODB_URI,
+  encKey,
+  sigKey,
 }
